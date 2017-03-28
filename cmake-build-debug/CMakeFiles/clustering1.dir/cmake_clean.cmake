5881a084bab78f99da7fc1bc6d40ebc7cc485657
henryzord <henry.cagnini@gmail.com>
2017-03-28 16:59:09 -0300

    Added cmake and main module

---
+++ cmake-build-debug/CMakeFiles/clustering1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clustering1.dir/main.c.o"
+  "clustering1.pdb"
+  "clustering1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clustering1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
